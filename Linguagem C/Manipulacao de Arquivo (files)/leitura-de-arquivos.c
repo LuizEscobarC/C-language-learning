e8bd531513de0975220753970d1db05ad817e8e6
Luiz  Escobar <54407649+LuizEscobarC@users.noreply.github.com>
2020-07-12 02:06:20 -0400

    update

--- Linguagem C/Manipulacao de Arquivo (files)/leitura-de-arquivos.c
+++ Linguagem C/Manipulacao de Arquivo (files)/leitura-de-arquivos.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main ()
 {
     FILE *arq;
     char frase[50];
-    arq = fopen("arquivo.txt", "r");
+    arq = fopen("arquivo.txt", "a+");
     if (arq == NULL)
     {
         printf("O arquivo não foi aberto. Error!!!\n");
     }
     else
     {
+    	//! transforma algo verdadeiro em falso, logo, enquanto o arquivo não chegar no fim, continue (a função retorna 0 se chegar no fim)
         while (!feof(arq))
         {
             fgets(frase, 50, arq);
@@ -27,4 +29,7 @@ int main ()
         }
     }
     fclose(arq);
+    system("pause"); //acrescentei isso pq os professores não perceberam que o executavel abria e logo fechava pois não tinha um "pause"
+     return(0);
+     //para funcionar,execute uma vez e dps escreva algo dentro do arquivo que foi gerado
 }
