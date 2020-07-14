b5dda3e2ecf70bd67c365742487aeba32615d14e
Luiz  Escobar <54407649+LuizEscobarC@users.noreply.github.com>
2020-07-13 21:19:20 -0400

    Create Converte-Peso-Libras.c

---
+++ Linguagem C/Funções/Converte-Peso-Libras.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+float convertep(float peso);
+
+int main()
+{
+    float peso, resposta;
+    printf("informe o peso em quilogramas: ");
+    scanf ("%f", &peso);
+    resposta = convertp(peso);
+    printf("O peso %.2f em libras é %.2f\n", peso, resposta);
+}
+floar convertep(float peso)
+{
+    peso = peso * 2.68;
+    return peso;
+}
