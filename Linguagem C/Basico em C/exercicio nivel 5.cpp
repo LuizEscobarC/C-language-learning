aa7041e7e6c9dd555b6742fccb13052c359fd63a
Luiz  Escobar <54407649+LuizEscobarC@users.noreply.github.com>
2020-07-16 00:35:08 -0400

    documentação
    documentação

--- Linguagem C/Basico em C/exercicio nivel 5.cpp
+++ Linguagem C/Basico em C/exercicio nivel 5.cpp
@@ -1,15 +1,16 @@
 #include<stdio.h>
 #include<math.h>
-
+//apresentação de como declarar uma constante
 #define pi 3.14
-//nivel 1
+
 int main(){
 	float raio, area, perimetro;
 	printf("Digite o raio: ");
 	scanf("%f",&raio);
+	//utilização de aritmetica e de uma função intrinseca (pow)
 	area = pi*(pow(raio,2));
 	perimetro = 2*pi*(pow(raio,2));
-	printf("A area do circulo do raio %.2f � %.2f e o perimentro � %.2f",raio,area,perimetro);
+	printf("A area do circulo do raio %.2f é %.2f e o perimentro é %.2f",raio,area,perimetro);
 	return(0);
 }
 
--- Linguagem C/Estruta condicional e de Repetição/exercicio nivel 14 Repetição e If (Aqui se usa a comparação de string minuscula ou maiuscula).cpp
+++ Linguagem C/Estruta condicional e de Repetição/exercicio nivel 14 Repetição e If (Aqui se usa a comparação de string minuscula ou maiuscula).cpp
@@ -4,16 +4,19 @@
 #include<string.h>
 int main()
 {
-	//nivel 2
+//utilizando um set de linguagem, nesse caso em portugues
 	setlocale(LC_ALL,"portuguese");
-	
-	Areacomodo = 0;
-	Areatotal = 0;	
+//aqui eu já declaro as variáveis com valor
 	char nome_comodo[30];
-	float largura, comprimento, Areacomodo, Areatotal;
+	float largura, comprimento, Areacomodo = 0 , Areatotal = 0;
+		
 	printf("Digite o nome do comodo:");
+/*nessa parte eu utilizei o espaço antes do
+formatador %s para o compilador entender
+que não é pra ler enter do buffer stdin*/
 	scanf(" %s", nome_comodo);
-	
+	//pode dar erro se o nome comodo for digitado SiM OU sIm OU sIM OU Sim OU SIm.
+	//existe uma função na string.h que pega a variavel e retorna maiusculo.
 	while(strcmp(nome_comodo,"fim") != 0 && strcmp(nome_comodo,"FIM" ) != 0) 
 	{
 		printf("Digite a largura do comodo: \n");
@@ -27,7 +30,7 @@ int main()
 		scanf(" %s", nome_comodo);
 	 } 
 	 system("cls");
-	 printf("\n\nA area total da casa � %.2f\n\n", Areatotal);
+	 printf("\n\nA area total da casa é %.2f\n\n", Areatotal);
 	 return(0);
 }
 
