9cee87f72a2243ad3f5bad15012fd676d8ea2c2f
Luiz  Escobar <54407649+LuizEscobarC@users.noreply.github.com>
2020-07-16 00:40:17 -0400

    Documentação

--- Linguagem C/Funções/retorna-quant.consoantes.c
+++ Linguagem C/Funções/retorna-quant.consoantes.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <string.h>
-//nivel 3
+//entra parametro vetor por valor 
 int conta (char nome[])
 {
+    //variaveis locais
     int i, tam, qtd;
-
+    //função que retorna o tamanho da variável em int 
     tam = strlen (nome);
+    //função que retorna a string maiuscula
     nome = strupr(nome);
     qtd = 0;
+    //percorre o vetor
     for (i = 0; i < tam; i++)
     {
+        //se for diferente das vogais cont!
         if ((nome[i] != 'A')&&(nome[i] != 'E')&&(nome[i] != 'I')&&(nome[i] != 'O')&&(nome[i] != 'U')&&(nome[i] != ' '))
             qtd++;
     }
@@ -22,5 +26,6 @@ int main()
     printf("informe a string: ");
     gets(palavra);
     total = conta(palavra);
+    // EEHH TAH DANH!!! ai está a quantidade de consoates de string e a palavra
     total("A quantidade de consoantes da string %s é %d\n", palavra,total);
 }
