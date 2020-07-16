e793b8d3c30524e68d6b3f62b7808b0138da1f80
Luiz  Escobar <54407649+LuizEscobarC@users.noreply.github.com>
2020-07-15 23:52:09 -0400

    Update exercicio nivel 5.cpp

--- Linguagem C/Basico em C/exercicio nivel 5.cpp
+++ Linguagem C/Basico em C/exercicio nivel 5.cpp
@@ -2,13 +2,14 @@
 #include<math.h>
 
 #define pi 3.14
+//nivel 1
 int main(){
 	float raio, area, perimetro;
 	printf("Digite o raio: ");
 	scanf("%f",&raio);
 	area = pi*(pow(raio,2));
 	perimetro = 2*pi*(pow(raio,2));
-	printf("A area do circulo do raio %.2f é %.2f e o perimentro é %.2f",raio,area,perimetro);
+	printf("A area do circulo do raio %.2f ï¿½ %.2f e o perimentro ï¿½ %.2f",raio,area,perimetro);
 	return(0);
 }
 
