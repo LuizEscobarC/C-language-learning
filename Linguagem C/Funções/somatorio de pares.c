cff86cbc5b5fd4c7078d930fae1befb51e3d2311
Luiz  Escobar <54407649+LuizEscobarC@users.noreply.github.com>
2020-07-18 00:20:58 -0400

    creat and doc

--- Linguagem C/Funções/somatorio de pares.c
+++ Linguagem C/Funções/somatorio de pares.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//retorna o somatorio dos numeros pares até o numero digitado
 int somatorio_par (int N);
 
 int main()
@@ -10,15 +10,20 @@ int main()
     scanf("%d", &numero);
     numero = somatorio_par(numero);
     printf("O somatorio entre os numeros pares do numero que digitou é %d\n",numero);
+    system("pause");
 }
 
 int somatorio_par (int N)
 {
-    int i;
-    for (i = 1; i <= N; i++ )
+    int i, S;
+    S = 0;
+    
+    for (i = 1; i <= N; i++)
     {
-        if (N % 2 == 0)
-            N = N + i;
+    	//soma somente os pares
+        if (i % 2 == 0)
+            S = S + i;
     }
-    return N;
+    //retorna o somatório
+    return S;
 }
