7f98dc50427d3aaf5bb2ff539b90b597c4b6e704
Luiz  Escobar <54407649+LuizEscobarC@users.noreply.github.com>
2020-07-20 23:58:30 -0400

    update == ordenação
    Adicionei uma função de ordena do menor para o maior

---
+++ Linguagem C/PROJETO-CLIENTE/projeto-list 4.0.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+typedef struct
+{
+	int codigo;
+	char nome[31];
+	int telefone;
+	char email[51];
+}	Cliente;
+
+typedef struct
+{
+	int limite;
+	int total;
+
+
+	Cliente	cl[30];
+}	Cadastro;
+
+void imprimirLista(Cadastro*);
+int  novoCadastro(Cadastro*);
+int ordenar(Cadastro*);
+//main, por enquanto somente duas funções
+int main()
+{
+	setlocale(LC_ALL,"portuguese");
+	Cadastro	cad;
+
+	novoCadastro(&cad);
+	imprimirLista(&cad);
+	ordenar(&cad);
+	system("pause");
+	return 0;
+};
+//função que imprime tudo na tela 
+void imprimirLista(Cadastro* cad)
+{
+	int i;
+	printf("cadastro tem %d de %d clientes\n\n",
+		cad->total, cad->limite);
+	for ( i = 0; i < cad->total; i += 1){ // i = i + 1;
+		printf("\t-----> %d\n", cad->cl[i].codigo);
+		printf("\t%s\n", cad->cl[i].nome);
+		printf("\t%d\n", cad->cl[i].telefone);
+		printf("\t%s\n", cad->cl[i].email);	
+	}
+	return;
+}
+//função que cadastra clientes 
+//por enquanto somente tem memória temporária
+int novoCadastro(Cadastro* cad)
+{
+	Cliente modelo;
+	int i, opcao;
+	//cadastro por quantidade
+	printf("Quantos clientes deseja cadastrar?");
+	scanf("%d",& opcao);
+	for (i=0; i < opcao; i++)
+	{
+		printf("\ndigite o código: ");
+		scanf("%d",&modelo.codigo);
+		fflush(stdin);
+		printf("\ndigite o nome: ");
+		gets(modelo.nome);
+		printf("\nDigite o telefone: ");
+		scanf("%d",&modelo.telefone);
+		printf("\nDigite o email: ");
+		fflush(stdin);
+		gets(modelo.email);
+		cad->cl[i] = modelo;
+	}
+	cad->total = i;
+	cad->limite = 30;
+	return 1;
+};
+// ordena do menor para o maior para função imprimirLista
+int ordenar (Cadastro* cad)
+{
+	int i, j;
+	Cliente troca;
+	for (i = 0; i < 29; i++)
+	{
+		for (j = i + 1; j < 30; j++)
+		{
+			if (cad->cl[i] > cad->cl[j])
+			{
+				troca = cad->cl[j];
+				cad->cl[j] = cad->cl[i];
+				cad->cl[i] = troca;
+			}
+		}
+	}
+}
